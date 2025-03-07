3044785837c0eee7c434ec287ff31ad9408dfd3a
Dan <dan.dev947366@gmail.com>
2025-03-07 22:22:49 +0200

    feat(db): add db connection

--- server/server.js
+++ server/server.js
@@ -1,19 +1,28 @@
-import express from "express"
-import morgan from "morgan"
-import cors from "cors"
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
+import { DATABASE } from "./config.js";
 
-const app = express()
+const app = express();
+//db
+mongoose
+  .connect(DATABASE)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
 
 //middleware
-app.use(express.json())
-app.use(morgan("dev"))
-app.use(cors())
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
 // routes
-app.get("/api", (req, res)=> {
-    res.json({
-        data: "Hello from nodejs api"
-    })
-})
+app.get("/api", (req, res) => {
+  res.json({
+    data: "Hello from nodejs api"
+  });
+});
 
-app.listen(8000, () => {"Listening on port 8000...."})
+app.listen(8000, () => {
+  "Listening on port 8000....";
+});
