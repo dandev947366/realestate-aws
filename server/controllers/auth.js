0b2c82cdb862c32231f3ead09ae59d576e514cf0
Dan <dan.dev947366@gmail.com>
2025-03-08 09:23:54 +0200

    feat(email): add email template

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,61 +1,46 @@
-import * as config from "../config.js"
-import jwt from 'jsonwebtoken'
+import * as config from "../config.js";
+import jwt from "jsonwebtoken";
+import { emailTemplate } from "../helpers/email.js";
 export const welcome = (req, res) => {
-    res.json({
-      data: "Hello from nodejs api auth"
-    });
+  res.json({
+    data: "Hello from nodejs api auth"
+  });
 };
 
 export const preRegister = async (req, res) => {
-  try{
-    const {email, password} = req.body
-    const token = jwt.sign({email, password}, config.JWT_SECRET, {
+  try {
+    const { email, password } = req.body;
+    const token = jwt.sign({ email, password }, config.JWT_SECRET, {
       expiresIn: "1h" //1 hour -options: 1w 1y
-    })
-    config.AWSSES.sendEmail({
-      Source: config.EMAIL_FROM,
-      Destination: {
-        ToAddresses: ["danle947366@gmail.com"]
-      },
-      Message: {
-        Body: {
-          Html: {
-            Charset: "UTF-8",
-            Data: `
-            <html>
-              <h1>Welcome to Real Estate AWS</h1>
-              <p>Please click the link below to activate your account</p>
-              <a href="${config.DEV_CLIENT_URL}/auth/account-activate/${token}">Activate my account</a>
-            </html>
-            `
-          }
-        },
-        Subject: {
-          Charset: "UTF-8",
-          Data: "Welcome to Real Estate AWS"
+    });
+    config.AWSSES.sendEmail(
+      emailTemplate(
+        email,
+        `<p>Please click the link below to activate your account</p>
+                      <a href="${config.DEV_CLIENT_URL}/auth/account-activate/${token}">Activate my account</a>`,
+        config.REPLY_TO,
+        "Activate your account"
+      ),
+      (err, data) => {
+        if (err) {
+          console.log(err);
+          return res.json({ ok: false, error: err.message });
+        } else {
+          return res.json({ ok: true, message: "Email sent successfully!" });
         }
       }
-    }, (err, data)=>{
-      if(err){
-        console.log(err)
-        return res.json({ok:false, error: err.message })
-      }else{
-        return res.json({ok:true, message: "Email sent successfully!" })
-      }
-    })
+    );
+  } catch (err) {
+    console.log(err);
+    return res.json({ error: "Somthine went wrong. Try again." });
   }
-  catch(err){
-    console.log(err)
-    return res.json({error: "Somthine went wrong. Try again."})
-  }
-}
+};
 
-export const register = async(req, res) => {
+export const register = async (req, res) => {
   try {
-    console.log(req.body)
-    
-  } catch(err) {
-    console.log(err)
-    return res.json({error: "Somthine went wrong. Try again."})
+    console.log(req.body);
+  } catch (err) {
+    console.log(err);
+    return res.json({ error: "Somthine went wrong. Try again." });
   }
-}
+};
