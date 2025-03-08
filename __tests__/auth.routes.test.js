e394bad17debbea877f64d6ec83c1cdcde4c66f8
Dan <dan.dev947366@gmail.com>
2025-03-08 21:13:21 +0200

    fix(test): update test

--- __tests__/auth.routes.test.js
+++ __tests__/auth.routes.test.js
@@ -3,9 +3,10 @@ import * as chai from 'chai';
 const expect = chai.expect; 
 import { app, server } from "../server.js";
 describe('GET /', () => {
-  after(() => {
-    server.close(); 
-    console.log("Server close.")
+  after(async () => {
+    await server.close(); 
+    await mongoose.disconnect();
+    console.log("Server close. DB disconnected.")
   });
   it('should return an error with an invalid token', async () => {
     const response = await request(app)
