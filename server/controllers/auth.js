58823c86eb5af41be60839adb649a86e6501bee9
Dan <dan.dev947366@gmail.com>
2025-03-08 10:56:13 +0200

    feat(auth): add validator for email , password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,6 +4,7 @@ import { emailTemplate } from "../helpers/email.js";
 import { hashPassword, comparePassword } from "../helpers/auth.js";
 import User from '../models/user.js';
 import { nanoid } from "nanoid";
+import validator from 'email-validator';
 export const welcome = (req, res) => {
   res.json({
     data: "Hello from nodejs api auth"
@@ -13,6 +14,20 @@ export const welcome = (req, res) => {
 export const preRegister = async (req, res) => {
   try {
     const { email, password } = req.body;
+    // validation
+    if (!validator.validate(email)){
+      return res.json({error:"A valid email is required"})
+    }
+    if (!password){
+      return res.json({error: "Password is required"})
+    }
+    if (password && password?.length < 6) {
+      return res.json({error: "Password should be at least 6 characters"})
+    }
+    const user = await User.findOne({email})
+    if(user) {
+      return res.json({error: "Email is taken"})
+    }
     const token = jwt.sign({ email, password }, config.JWT_SECRET, {
       expiresIn: "1h" //1 hour -options: 1w 1y
     });
@@ -42,6 +57,11 @@ export const preRegister = async (req, res) => {
 export const register = async (req, res) => {
   try {
     const {email, password} = jwt.verify(req.body.token, config.JWT_SECRET)
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: "Email is already registered. Please log in." });
+    }
     const hashedPassword = await hashPassword(password)
     const user = await new User({
       username: nanoid(6),
